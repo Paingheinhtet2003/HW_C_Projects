e4b90df90ef10dac8d738e94be60cb2076dcdad7
Paing Hein Htet <135322735+Paingheinhtet2003@users.noreply.github.com>
2024-11-11 15:10:10 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void descendingSort(int [], int);
+int maxIndex(int [], int);
+
+int main()
+{
+    int size, arr[size];
+
+    printf("Enter the size of the array: \t");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the array: \n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[size]);
+    }
+
+    descendingSort(arr, size);
+    printf("Sorted Array in descending order: \n");
+    for(int i = 0; i< size; i++) {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+
+    int max_index = maxIndex(arr, size);
+    printf("Index of the largest element: %d.\n", max_index);
+
+    return 0;
+}
+
+void descendingSort(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int maxIndex(int arr[], int size) {
+    int max_index = 0;
+    for(int i = 0; i < size; i++) {
+        if (arr[i] > arr[max_index]) {
+            max_index = i;
+        }
+    }
+    return max_index;
+}
