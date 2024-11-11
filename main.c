b33bf38140c56eb2e7a82e7f490b60facfe944cd
Paing Hein Htet <135322735+Paingheinhtet2003@users.noreply.github.com>
2024-11-11 15:15:45 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,20 +6,22 @@ int maxIndex(int [], int);
 
 int main()
 {
-    int size, arr[size];
+    int size;
 
     printf("Enter the size of the array: \t");
     scanf("%d", &size);
+    
+    int arr[size];
 
     printf("Enter the elements of the array: \n");
     for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[size]);
+        scanf("%d", &arr[i]);
     }
 
     descendingSort(arr, size);
     printf("Sorted Array in descending order: \n");
-    for(int i = 0; i< size; i++) {
-        printf("%d", arr[i]);
+    for(int i = 0; i<  size; i++) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
 
